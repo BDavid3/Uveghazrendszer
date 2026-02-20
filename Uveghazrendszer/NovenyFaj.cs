3d314329492cf80caa0d061967e19e3a346d1936
gubamagos <gubamagos@gmail.com>
2026-02-20 09:26:34 +0100

    növényfaj osztály kész

--- Uveghazrendszer/NovenyFaj.cs
+++ Uveghazrendszer/NovenyFaj.cs
@@ -8,5 +8,36 @@ namespace Uveghazrendszer
 {
 	internal class NovenyFaj
 	{
-	}
+		string nev;
+		int nedvessegTartalom; // 100 ideális
+		int egeszsegSzint; // 100 max
+		int optimalisSuruseg;
+
+		public NovenyFaj(string nev, int optimalisSuruseg)
+		{
+			this.nev = nev;
+			this.optimalisSuruseg = optimalisSuruseg;
+			this.nedvessegTartalom = 100;
+			this.egeszsegSzint = 100;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int NedvessegTartalom { get => nedvessegTartalom; set => nedvessegTartalom = value; }
+		public int EgeszsegSzint { get => egeszsegSzint; set => egeszsegSzint = value; }
+		public int OptimalisSuruseg { get => optimalisSuruseg; set => optimalisSuruseg = value; }
+
+		public string Azonosito
+		{
+			get
+			{
+				return this.nev.Substring(0, 3);
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{this.nev} nedvesseg: {this.nedvessegTartalom} egeszseg: {this.egeszsegSzint} idealis egyedszam: {this.optimalisSuruseg}";
+
+
+		}
 }
